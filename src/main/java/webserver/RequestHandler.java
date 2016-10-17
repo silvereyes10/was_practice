b90e3252776ae17c0d569abe5e2867435d643fae
namseh.lee <namseh.lee@nhn.com>
2016-10-17 14:09:57 +0900

    apache library 추가, urlParsing 로직 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,11 +1,15 @@
 package webserver;
 
+import com.google.common.collect.Maps;
+import org.apache.commons.collections.MapUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -30,15 +34,24 @@ public class RequestHandler extends Thread {
                 log.debug("[Request Header] {}", line);
 
                 if (line.startsWith("GET") || line.startsWith("POST")) {
-                    url = getRequestUrl(line);
+                    Map<String, Object> parsingMap = urlParsing(line);
+                    url = MapUtils.getString(parsingMap, "requestPath");
+                    log.info("[REQUEST URL] : {}", url);
+
+                    if (url.startsWith("/user/create")) {
+                        log.info("create!!!");
+
+                    } else {
+                        DataOutputStream dos = new DataOutputStream(out);
+                        byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+                        response200Header(dos, body.length);
+                        responseBody(dos, body);
+                    }
                 }
+
                 line = br.readLine();
             }
 
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-            response200Header(dos, body.length);
-            responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
@@ -64,9 +77,22 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private String getRequestUrl(String headerInfo) {
-        String[] tokens = headerInfo.split(" ");
+    private Map<String, Object> urlParsing(String line) {
+        String[] tokens = line.split(" ");
 
-        return tokens[1];
+        Map<String, Object> resultMap = Maps.newHashMap();
+        int index = tokens[1].indexOf("?");
+
+        if (index == -1) {
+            resultMap.put("requestPath", tokens[1]);
+        } else {
+            String requestPath = tokens[1].substring(0, index);
+            resultMap.put("requestPath", requestPath);
+
+            String params = tokens[1].substring(index + 1, tokens[1].length());
+            resultMap.put("params", HttpRequestUtils.parseQueryString(params));
+        }
+
+        return resultMap;
     }
 }
